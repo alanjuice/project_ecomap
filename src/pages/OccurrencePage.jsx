a3a7ab57bc8f49c20a602a9b86be11ba0f379ebc
alanjuice <alnjoze@gmail.com>
2025-01-01 09:39:34 +0530

    add mock data for testing

--- src/pages/OccurrencePage.jsx
+++ src/pages/OccurrencePage.jsx
@@ -2,12 +2,14 @@ import FilterContent from "../components/FilterContent";
 import CardGrid from "../components/CardGrid";
 
 const OccurencePage = () => {
-  return <>
-    <div className="flex ">
-        <FilterContent/>
-        <CardGrid/>
-      </div>
-  </>;
+    return (
+        <>
+            <div className="flex ">
+                <FilterContent />
+                <CardGrid />
+            </div>
+        </>
+    );
 };
 
 export default OccurencePage;
--- src/pages/SpeciesPage.jsx
+++ src/pages/SpeciesPage.jsx
@@ -1,15 +1,18 @@
 import CardGrid from "../components/CardGrid";
 import FilterContent from "../components/FilterContent";
+import { speciesMinimialData } from "../utils/mockData";
 
 const SpeciesPage = () => {
-  return (
-    <>
-      <div className="flex ">
-        <FilterContent />
-        <CardGrid/>
-      </div>
-    </>
-  );
+    const speciesData = speciesMinimialData;
+
+    return (
+        <>
+            <div className="flex ">
+                <FilterContent />
+                <CardGrid data={speciesData} />
+            </div>
+        </>
+    );
 };
 
 export default SpeciesPage;
