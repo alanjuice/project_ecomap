656fa7a2ee879100bd69d14e095a33fbbf361d3f
alanjuice <alnjoze@gmail.com>
2025-01-01 09:38:46 +0530

    change SpeciesCard to reusable component

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,68 +1,20 @@
-import SpeciesCard from "./SpeciesCard";
+import { Link } from "react-router-dom";
+import ImageCard from "./ImageCard";
 
-const speciesData = [
-  {
-    label: "Tiger",
-  },
-  {
-    label: "Peguin",
-  },
-  {
-    label: "Lion",
-  },
-  {
-    label: "Mumal",
-  },
-  {
-    label: "Fish",
-  },{
-    label: "Fish",
-  },{
-    label: "Fish",
-  },{
-    label: "Fish",
-  },{
-    label: "Fish",
-  },{
-    label: "Fish",
-  },{
-    label: "Fish",
-  },,{
-    label: "Fish",
-  },,{
-    label: "Fish",
-  },,{
-    label: "Fish",
-  },,{
-    label: "Fish",
-  },,{
-    label: "Fish",
-  },,{
-    label: "Fish",
-  },,{
-    label: "Fish",
-  },,{
-    label: "Fish",
-  },,{
-    label: "Fish",
-  },,{
-    label: "Fish",
-  },,{
-    label: "Fish",
-  }]
-
-
-const CardGrid = () => {
+const CardGrid = ({ data }) => {
+    console.log(data);
     return (
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-3 p-4">
-        {speciesData.map((species, index) => (
-          <SpeciesCard
-            key={index}
-            imageUrl={species.imageUrl || "https://placehold.co/400"}
-            label={species.label}
-          />
-        ))}
-      </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-3 p-4">
+            {data.map((item, index) => (
+                <Link to={"/species/" + item.id}>
+                    <ImageCard
+                        key={index}
+                        imageUrl={item.imageUrl || "https://placehold.co/400"}
+                        label={item.label}
+                    />
+                </Link>
+            ))}
+        </div>
     );
 };
 
