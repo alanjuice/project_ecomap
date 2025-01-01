de2e5ea6c2d62997111b4b80c77c5dab95c732ca
alanjuice <alnjoze@gmail.com>
2025-01-01 11:46:15 +0530

    add occurence details page

--- src/pages/OccurrencePage.jsx
+++ src/pages/OccurrencePage.jsx
@@ -1,12 +1,13 @@
 import FilterContent from "../components/FilterContent";
 import CardGrid from "../components/CardGrid";
+import { occurenceMinimalData } from "../utils/mockData";
 
 const OccurencePage = () => {
     return (
         <>
             <div className="flex ">
                 <FilterContent />
-                <CardGrid />
+                <CardGrid data={occurenceMinimalData} resource={"occurence"} />
             </div>
         </>
     );
