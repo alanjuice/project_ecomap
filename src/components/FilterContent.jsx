a96a854c18d3837af1fb29a82b967d120a636fc5
alanjuice <alnjoze@gmail.com>
2025-01-13 19:40:12 +0530

    fix sidebar and add style

--- src/components/FilterContent.jsx
+++ src/components/FilterContent.jsx
@@ -1,121 +1,130 @@
 import { useEffect, useState } from "react";
 
-const FilterContent = ({ type,viewFilter }) => {
-  const conservationStatuses = [
-    "Endangered",
-    "Vulnerable",
-    "Near Threatened",
-    "Least Concern",
-    "Critically Endangered",
-  ];
+const FilterContent = ({ type, viewFilter }) => {
+    const conservationStatuses = [
+        "Endangered",
+        "Vulnerable",
+        "Near Threatened",
+        "Least Concern",
+        "Critically Endangered",
+    ];
 
-  const [filterStyle, setFilterStyle] = useState(viewFilter ? "" : "hidden");
+    const [filterStyle, setFilterStyle] = useState(viewFilter ? "" : "hidden");
 
-  useEffect(() => {
-    setFilterStyle(viewFilter ? "" : "hidden");
-  }, [viewFilter]);
+    useEffect(() => {
+        setFilterStyle(viewFilter ? "" : "hidden");
+    }, [viewFilter]);
 
+    // State for sorting options
+    const [sortOption, setSortOption] = useState(
+        type === "occurrence" ? "recent" : "alphabetic"
+    );
 
-  // State for sorting options
-  const [sortOption, setSortOption] = useState(
-    type === "occurrence" ? "recent" : "alphabetic"
-  );
+    // Handle sorting option change
+    const handleSortChange = (e) => {
+        setSortOption(e.target.value);
+    };
 
-  // Handle sorting option change
-  const handleSortChange = (e) => {
-    setSortOption(e.target.value);
-  };
-
-  return (
-    <div className={`h-screen bg-gray-100 p-4 space-y-6  sm:block md:block ${filterStyle}`}>
-      <div className="text-xl font-bold text-gray-700">Filter</div>
-
-      {/* Conservation Status Selector for Species */}
-      {type === "species" && (
-        <div>
-          <h3 className="text-lg font-semibold text-gray-700 mb-2">
-            Conservation Status
-          </h3>
-          <div className="space-y-2">
-            {conservationStatuses.map((status, index) => (
-              <div key={index} className="flex items-center space-x-2">
-                <input
-                  type="radio"
-                  id={status}
-                  name="conservationStatus"
-                  className="h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
-                />
-                <label htmlFor={status} className="text-gray-700 font-medium">
-                  {status}
-                </label>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-
-      {/* Time Range For Occurrences */}
-      {type === "occurrence" && (
-        <div>
-          <h3 className="text-lg font-semibold text-gray-700 mb-2">
-            Time Range
-          </h3>
-          <div className="flex flex-col">
-            <div className="flex flex-col">
-              <label
-                htmlFor="startDate"
-                className="text-gray-700 font-medium mb-1"
-              >
-                Start Date
-              </label>
-              <input
-                type="date"
-                id="startDate"
-                className="p-2 border border-gray-300 rounded focus:ring-blue-500 focus:outline-none"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label
-                htmlFor="endDate"
-                className="text-gray-700 font-medium mb-1"
-              >
-                End Date
-              </label>
-              <input
-                type="date"
-                id="endDate"
-                className="p-2 border border-gray-300 rounded focus:ring-blue-500 focus:outline-none"
-              />
-            </div>
-          </div>
-        </div>
-      )}
-
-      {/* Sorting Options */}
-      <div>
-        <h3 className="text-lg font-semibold text-gray-700 mb-2">Sort By</h3>
-        <select
-          value={sortOption}
-          onChange={handleSortChange}
-          className="w-full p-2 border border-gray-300 rounded focus:ring-blue-500 focus:outline-none"
+    return (
+        <div
+            className={`h-screen bg-gray-100 p-4 space-y-6 block md:block ${
+                viewFilter ? "" : "sm:hidden"
+            } ${filterStyle}`}
         >
-          {type === "occurrence" ? (
-            <>
-              <option value="recent">Recently Added</option>
-              <option value="old">Oldest First</option>
-            </>
-          ) : (
-            <>
-              <option value="alphabetic">Alphabetic (A-Z)</option>
-              <option value="descending">Alphabetic (Z-A)</option>
-            </>
-          )}
-        </select>
-      </div>
+            <div className="text-xl font-bold text-gray-700">Filter</div>
 
+            {/* Conservation Status Selector for Species */}
+            {type === "species" && (
+                <div>
+                    <h3 className="text-lg font-semibold text-gray-700 mb-2">
+                        Conservation Status
+                    </h3>
+                    <div className="space-y-2">
+                        {conservationStatuses.map((status, index) => (
+                            <div
+                                key={index}
+                                className="flex items-center space-x-2"
+                            >
+                                <input
+                                    type="radio"
+                                    id={status}
+                                    name="conservationStatus"
+                                    className="h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
+                                />
+                                <label
+                                    htmlFor={status}
+                                    className="text-gray-700 font-medium"
+                                >
+                                    {status}
+                                </label>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
 
-    </div>
-  );
+            {/* Time Range For Occurrences */}
+            {type === "occurrence" && (
+                <div>
+                    <h3 className="text-lg font-semibold text-gray-700 mb-2">
+                        Time Range
+                    </h3>
+                    <div className="flex flex-col">
+                        <div className="flex flex-col">
+                            <label
+                                htmlFor="startDate"
+                                className="text-gray-700 font-medium mb-1"
+                            >
+                                Start Date
+                            </label>
+                            <input
+                                type="date"
+                                id="startDate"
+                                className="p-2 border border-gray-300 rounded focus:ring-blue-500 focus:outline-none"
+                            />
+                        </div>
+                        <div className="flex flex-col">
+                            <label
+                                htmlFor="endDate"
+                                className="text-gray-700 font-medium mb-1"
+                            >
+                                End Date
+                            </label>
+                            <input
+                                type="date"
+                                id="endDate"
+                                className="p-2 border border-gray-300 rounded focus:ring-blue-500 focus:outline-none"
+                            />
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {/* Sorting Options */}
+            <div>
+                <h3 className="text-lg font-semibold text-gray-700 mb-2">
+                    Sort By
+                </h3>
+                <select
+                    value={sortOption}
+                    onChange={handleSortChange}
+                    className="w-full p-2 border border-gray-300 rounded focus:ring-blue-500 focus:outline-none"
+                >
+                    {type === "occurrence" ? (
+                        <>
+                            <option value="recent">Recently Added</option>
+                            <option value="old">Oldest First</option>
+                        </>
+                    ) : (
+                        <>
+                            <option value="alphabetic">Alphabetic (A-Z)</option>
+                            <option value="descending">Alphabetic (Z-A)</option>
+                        </>
+                    )}
+                </select>
+            </div>
+        </div>
+    );
 };
 
 export default FilterContent;
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,27 +2,34 @@ import SearchBar from "./SearchBar";
 import FilterContent from "./FilterContent";
 import { useState } from "react";
 
+import FilterIcon from "../assets/filter.png";
+import CancelIcon from "../assets/cancel.png";
+
 const SideBar = ({ type }) => {
-  const [mobileFilterVisible, setMobileFilterVisible] = useState(false);
+    const [mobileFilterVisible, setMobileFilterVisible] = useState(false);
 
-  return (
-    <div className="md:h-screen bg-gray-100 shadow-lg p-2 sticky top-0 sm:block h-24">
-      <div className="flex flex-row">
-        <SearchBar />
-        <button
-          className="md:hidden"
-          onClick={() => {
-            setMobileFilterVisible(!mobileFilterVisible);
-            console.log(mobileFilterVisible);
-          }}
-        >
-          View
-        </button>
-      </div>
+    return (
+        <div className="md:h-screen bg-gray-100 shadow-lg p-2 sticky top-0 sm:block h-24">
+            <div className="flex flex-row justify-evenly">
+                <SearchBar />
+                <button
+                    className="md:hidden"
+                    onClick={() => {
+                        setMobileFilterVisible(!mobileFilterVisible);
+                        console.log(mobileFilterVisible);
+                    }}
+                >
+                    <img
+                        src={mobileFilterVisible ? CancelIcon : FilterIcon}
+                        className="size-6"
+                        alt=""
+                    />
+                </button>
+            </div>
 
-      <FilterContent type={type} viewFilter={mobileFilterVisible} />
-    </div>
-  );
+            <FilterContent type={type} viewFilter={mobileFilterVisible} />
+        </div>
+    );
 };
 
 export default SideBar;
