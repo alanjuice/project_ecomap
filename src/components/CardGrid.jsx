987b3b629ebadbe1426813f3fd7160776218f877
alanjuice <alnjoze@gmail.com>
2025-01-16 19:51:26 +0530

    fix unique key for list childs error

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -5,10 +5,10 @@ const CardGrid = ({ data, resource }) => {
     console.log(data);
     return (
         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-3 p-4 grid-rows-3">
-            {data.map((item, index) => (
+            {data.map((item) => (
                 <Link to={"/" + resource + "/" + item.id}>
                     <ImageCard
-                        key={index}
+                        key={item.id}
                         imageUrl={item.imageUrl || "https://placehold.co/400"}
                         label={item.label}
                     />
