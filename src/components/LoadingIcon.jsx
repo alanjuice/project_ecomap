057abccd6db5c30aae026a9a202198548ce56f02
alanjuice <alnjoze@gmail.com>
2025-01-21 15:31:08 +0530

    add loading icon

---
+++ src/components/LoadingIcon.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Mosaic } from "react-loading-indicators";
+
+export default function LoadingIcon() {
+    return (
+        <div className="flex justify-center items-center h-screen w-full">
+            <Mosaic color={["#33CCCC", "#33CC36", "#B8CC33", "#FCCA00"]} />
+        </div>
+    );
+}
