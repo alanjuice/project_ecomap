c947033e96d0a144a20371cc96398f582d5fcbdb
alanjuice <alnjoze@gmail.com>
2025-01-21 15:33:08 +0530

    change property names

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -3,14 +3,13 @@ import ImageCard from "./ImageCard";
 
 const SpeciesCardGrid = ({ data, resource }) => {
     return (
-
         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-3 p-4 grid-rows-3">
             {data.map((item) => (
-                <Link to={"/" + resource + "/" + item.id}>
+                <Link to={"/" + resource + "/" + item._id}>
                     <ImageCard
                         key={item.id}
-                        imageUrl={item.imageUrl || "https://placehold.co/400"}
-                        label={item.common_name}
+                        imageUrl={item.image || "https://placehold.co/400"}
+                        label={item.title}
                     />
                 </Link>
             ))}
