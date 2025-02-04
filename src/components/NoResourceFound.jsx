4d2bba2e708e5c971f9984324937e6e1ec1b75fd
Alan <93069058+alanjuice@users.noreply.github.com>
2025-02-04 23:27:18 +0530

    customize component

--- src/components/NoResourceFound.jsx
+++ src/components/NoResourceFound.jsx
@@ -1,7 +1,18 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Ban } from "lucide-react";
+
 const NoResourceFound = ({ resource }) => {
     return (
-        <div className="flex items-center justify-center h-64 text-gray-500 text-xl font-semibold w-100">
-            No {resource} Found
+        <div className="flex items-center justify-center h-full w-full p-4">
+            <div className="flex flex-col items-center justify-center text-center">
+                <Alert variant="destructive" className="max-w-md">
+                    <Ban className="h-6 w-6 text-red-500 mx-auto" />
+                    <AlertTitle className="mt-2">No {resource} Found</AlertTitle>
+                    <AlertDescription>
+                        It looks like there are no {resource.toLowerCase()} available at the moment.
+                    </AlertDescription>
+                </Alert>
+            </div>
         </div>
     );
 };
