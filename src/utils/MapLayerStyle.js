b6d7be4d29c08d3c657325d90e275cbcab003c84
Alan <93069058+alanjuice@users.noreply.github.com>
2025-02-12 11:30:58 +0530

    Update MapLayerStyle.js

--- src/utils/MapLayerStyle.js
+++ src/utils/MapLayerStyle.js
@@ -2,62 +2,69 @@ const heatmapLayerStyle = {
     id: "species_heat",
     type: "heatmap",
     source: "species_locations",
-    maxzoom: 12,
+    maxzoom: 9,
     paint: {
-        "heatmap-intensity": ["interpolate", ["linear"], ["zoom"], 0, 1, 9, 3],
-
-        "heatmap-color": [
-            "interpolate",
-            ["linear"],
-            ["heatmap-density"],
-            0,
-            "rgba(33,102,172,0)",
-            0.2,
-            "rgb(103,169,207)",
-            0.4,
-            "rgb(209,229,240)",
-            0.6,
-            "rgb(253,219,199)",
-            0.8,
-            "rgb(239,138,98)",
-            1,
-            "rgb(178,24,43)",
-        ],
-
-        "heatmap-radius": ["interpolate", ["linear"], ["zoom"], 0, 2, 9, 20],
-
-        "heatmap-opacity": ["interpolate", ["linear"], ["zoom"], 7, 1, 9, 0],
+      "heatmap-weight": ["interpolate", ["linear"], ["get", "mag"], 0, 0, 6, 1],
+      "heatmap-intensity": ["interpolate", ["linear"], ["zoom"], 0, 1, 9, 3],
+      "heatmap-color": [
+        "interpolate",
+        ["linear"],
+        ["heatmap-density"],
+        0,
+        "rgba(33,102,172,0)",
+        0.2,
+        "rgb(103,169,207)",
+        0.4,
+        "rgb(209,229,240)",
+        0.6,
+        "rgb(253,219,199)",
+        0.8,
+        "rgb(239,138,98)",
+        1,
+        "rgb(178,24,43)",
+      ],
+      "heatmap-radius": ["interpolate", ["linear"], ["zoom"], 0, 2, 9, 20],
+      "heatmap-opacity": ["interpolate", ["linear"], ["zoom"], 7, 1, 9, 0],
     },
-};
-
-const pointLayerStyle = {
+  };
+  const pointLayerStyle = {
     id: "species-point",
     type: "circle",
     source: "species_locations",
-    minzoom: 12,
+    minzoom: 7,
     paint: {
-        "circle-color": [
-            "interpolate",
-            ["linear"],
-            ["get", "mag"],
-            1,
-            "rgba(33,102,172,0)",
-            2,
-            "rgb(103,169,207)",
-            3,
-            "rgb(209,229,240)",
-            4,
-            "rgb(253,219,199)",
-            5,
-            "rgb(239,138,98)",
-            6,
-            "rgb(178,24,43)",
-        ],
-        "circle-stroke-color": "white",
-        "circle-stroke-width": 1,
-        // Transition from heatmap to circle layer by zoom level
-        "circle-opacity": ["interpolate", ["linear"], ["zoom"], 7, 0, 8, 1],
+      // Size circle radius by earthquake magnitude and zoom level
+      "circle-radius": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        7,
+        ["interpolate", ["linear"], ["get", "mag"], 1, 1, 6, 4],
+        16,
+        ["interpolate", ["linear"], ["get", "mag"], 1, 5, 6, 50],
+      ],
+      // Color circle by earthquake magnitude
+      "circle-color": [
+        "interpolate",
+        ["linear"],
+        ["get", "mag"],
+        1,
+        "rgba(33,102,172,0)",
+        2,
+        "rgb(103,169,207)",
+        3,
+        "rgb(209,229,240)",
+        4,
+        "rgb(253,219,199)",
+        5,
+        "rgb(239,138,98)",
+        6,
+        "rgb(178,24,43)",
+      ],
+      "circle-stroke-color": "white",
+      "circle-stroke-width": 1,
+      // Transition from heatmap to circle layer by zoom level
+      "circle-opacity": ["interpolate", ["linear"], ["zoom"], 7, 0, 8, 1],
     },
-};
-
-export { pointLayerStyle, heatmapLayerStyle };
+  };
+  export {pointLayerStyle,heatmapLayerStyle}
